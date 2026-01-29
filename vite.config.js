25bc9c6dae27ee87f6faa4727f133f0e9b40b6c5
foisaluddin400 <foisalrk2@gmail.com>
2026-01-29 16:12:29 +0600

    adsf

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
   plugins: [react()],
